5c87714404f8d25c161b6851cffb13f27d85161d
JuliaMaidan <115953663+JuliaMaidan@users.noreply.github.com>
2024-02-29 20:08:30 +0200

    add script to reviews and change some styles

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,13 +1,47 @@
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   direction: 'gorizontal',
-//   width: 300,
-//   height: 500,
-//   //   loop: true,
+const reviewsContainer = document.querySelector('.reviews__list');
+const reviews = reviewsContainer.querySelectorAll('.reviews__item');
+const prevReview = document.querySelector('.prev__review');
+const nextReview = document.querySelector('.next__review');
 
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// });
+let currentIndex = 0;
+const maxIndex = reviews.length - 1;
+
+function updateUI() {
+  // Перевірка ширини вікна перегляду
+  if (window.innerWidth < 835) {
+    showReviews(1);
+  } else if (window.innerWidth < 1440) {
+    showReviews(2);
+  } else {
+    showReviews(4);
+  }
+}
+
+function showReviews(numberToShow) {
+  // Показати відповідну кількість відгуків, приховавши решту
+  reviews.forEach((review, index) => {
+    if (index < currentIndex || index >= currentIndex + numberToShow) {
+      review.style.display = 'none';
+    } else {
+      review.style.display = 'grid';
+    }
+  });
+}
+
+prevReview.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateUI();
+  }
+});
+
+nextReview.addEventListener('click', () => {
+  if (currentIndex < maxIndex) {
+    currentIndex++;
+    updateUI();
+  }
+});
+
+// Оновлення відображення при завантаженні сторінки та зміні розміру вікна
+window.addEventListener('load', updateUI);
+window.addEventListener('resize', updateUI);
