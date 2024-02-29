be34ab6293e88e304546114aa9a837f69865ae8f
JuliaMaidan <115953663+JuliaMaidan@users.noreply.github.com>
2024-02-29 20:08:30 +0200

    add script to reviews and change some styles

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -700,18 +700,40 @@ document.getElementById("prevSlide").addEventListener("click", prevSlide);
 document.getElementById("nextSlide").addEventListener("click", nextSlide);
 
 },{}],"fYIS4":[function(require,module,exports) {
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   direction: 'gorizontal',
-//   width: 300,
-//   height: 500,
-//   //   loop: true,
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// });
+const reviewsContainer = document.querySelector(".reviews__list");
+const reviews = reviewsContainer.querySelectorAll(".reviews__item");
+const prevReview = document.querySelector(".prev__review");
+const nextReview = document.querySelector(".next__review");
+let currentIndex = 0;
+const maxIndex = reviews.length - 1;
+function updateUI() {
+    // Перевірка ширини вікна перегляду
+    if (window.innerWidth < 835) showReviews(1);
+    else if (window.innerWidth < 1440) showReviews(2);
+    else showReviews(4);
+}
+function showReviews(numberToShow) {
+    // Показати відповідну кількість відгуків, приховавши решту
+    reviews.forEach((review, index)=>{
+        if (index < currentIndex || index >= currentIndex + numberToShow) review.style.display = "none";
+        else review.style.display = "grid";
+    });
+}
+prevReview.addEventListener("click", ()=>{
+    if (currentIndex > 0) {
+        currentIndex--;
+        updateUI();
+    }
+});
+nextReview.addEventListener("click", ()=>{
+    if (currentIndex < maxIndex) {
+        currentIndex++;
+        updateUI();
+    }
+});
+// Оновлення відображення при завантаженні сторінки та зміні розміру вікна
+window.addEventListener("load", updateUI);
+window.addEventListener("resize", updateUI);
 
 },{}]},["4rkIz","8lqZg"], "8lqZg", "parcelRequire1631")
 
