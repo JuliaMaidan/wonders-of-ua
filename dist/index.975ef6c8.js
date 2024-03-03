ce40a73a1b449e2f2c6ff5440e331deb92071459
JuliaMaidan <115953663+JuliaMaidan@users.noreply.github.com>
2024-03-03 17:05:38 +0200

    add some animation

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -745,32 +745,34 @@ window.addEventListener("load", updateUI);
 window.addEventListener("resize", updateUI);
 
 },{}],"l9hyy":[function(require,module,exports) {
-function submitForm(event) {
-    event.preventDefault(); // Щоб уникнути автоматичного відправлення форми
-    // Отримуємо значення полів форми
-    const name = document.getElementById("name").value;
-    const phone = document.getElementById("phone").value;
-    const message = document.getElementById("message").value;
-    // Отримуємо модальне вікно
-    const modal = document.getElementById("modal");
-    const modalContent = document.getElementById("modal-content");
-    // Встановлюємо вміст модального вікна з введеними даними
-    modalContent.innerHTML = `Name: ${name}<br>Phone: ${phone}<br>Message: ${message}`;
-    // Показуємо модальне вікно
-    modal.style.display = "block";
-}
-// Отримуємо кнопку для закриття модального вікна
-const closeBtn = document.getElementsByClassName("close")[0];
-// Додаємо обробник події для закриття модального вікна
-closeBtn.onclick = function() {
-    const modal = document.getElementById("modal");
-    modal.style.display = "none";
-};
-// Закриття модального вікна при натисканні поза ним
-window.onclick = function(event) {
-    const modal = document.getElementById("modal");
-    if (event.target === modal) modal.style.display = "none";
-};
+// function submitForm(event) {
+//   event.preventDefault(); // Щоб уникнути автоматичного відправлення форми
+//   // Отримуємо значення полів форми
+//   const name = document.getElementById('name').value;
+//   const phone = document.getElementById('phone').value;
+//   const message = document.getElementById('message').value;
+//   // Отримуємо модальне вікно
+//   const modal = document.getElementById('modal');
+//   const modalContent = document.getElementById('modal-content');
+//   // Встановлюємо вміст модального вікна з введеними даними
+//   modalContent.innerHTML = `Name: ${name}<br>Phone: ${phone}<br>Message: ${message}`;
+//   // Показуємо модальне вікно
+//   modal.style.display = 'block';
+// }
+// // Отримуємо кнопку для закриття модального вікна
+// const closeBtn = document.getElementsByClassName('close')[0];
+// // Додаємо обробник події для закриття модального вікна
+// closeBtn.onclick = function () {
+//   const modal = document.getElementById('modal');
+//   modal.style.display = 'none';
+// };
+// // Закриття модального вікна при натисканні поза ним
+// window.onclick = function (event) {
+//   const modal = document.getElementById('modal');
+//   if (event.target === modal) {
+//     modal.style.display = 'none';
+//   }
+// };
 
 },{}]},["4rkIz","8lqZg"], "8lqZg", "parcelRequire1631")
 
